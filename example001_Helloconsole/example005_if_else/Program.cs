13a0b5c4c8b8d090cbcbbdfb532cbade5b645384
Rus27lan <ruslan27.11.96@mail.ru>
2022-07-11 23:52:31 +0500

    Сделали программу

---
+++ example001_Helloconsole/example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользовател, ");
+string username = Console.ReadLine();
+
+if(username.ToLower == "маша")
+{
+    Console.WriteLine("Ура это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
